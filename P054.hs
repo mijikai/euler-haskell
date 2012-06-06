6f692636c853ffc5ee33ae6088ad0ebe86eb9729
Lester B <tkhdlstfl.l+git@gmail.com>
2012-06-06 17:35:28 +0800

    Remove group_by because Data.List already provide one

--- P054.hs
+++ P054.hs
@@ -16,15 +16,6 @@ conseq cards = helper (tail cards) (fst (head cards))
             | otherwise = helper (tail cards) next
             where next = fst $ head cards
 
-group_by :: (Eq b) => [a] -> (a -> b) -> [[a]]
-group_by (x:xs) key = helper (key x) xs [x] []
-    where helper curr list group all
-            | null list = (reverse ((reverse group):all))
-            | f == curr = helper curr rest (first:group) all
-            | otherwise = helper f rest [first] ((reverse group):all)
-            where first = head list 
-                  f = key first
-                  rest = tail list
 same_suit :: [(CardValue, Char)] -> Bool
 same_suit (x:xs) = helper xs
     where init = snd x
