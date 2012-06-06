5477db13b6dd51bf65ba54287c8c2bdac3be9d13
Lester B <tkhdlstfl.l+git@gmail.com>
2012-06-06 17:36:06 +0800

    Add `rank` function that checks the rank of cards Prototype for rank
    Add remaining implementation on rank
    
    Fix type compiler error on rank

--- P054.hs
+++ P054.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 data CardRank = High | OnePair | TwoPairs | ThreeKind
     | Straight | Flush | FullHouse | FourKind
     | StraightFlush | RoyalFlush
@@ -24,3 +26,27 @@ same_suit (x:xs) = helper xs
             | init /= current = False
             | otherwise = helper $ tail list
             where current = snd $ head list
+
+rank :: [(CardValue, Char)] -> (CardRank, Int)
+rank cards@(_:_:_:_:_:[])
+    | conseq cards = rank_conseq cards
+    | same_suit cards = (Flush, value)
+    | otherwise = rank_group $ sortBy (compare `on` ((0-) . length)) $ groupBy ((==) `on` fst) cards
+    where value = fst $ head cards
+          card_length = length cards
+          rank_conseq cards
+            | same_suit cards = if value == Ace
+                                then (RoyalFlush, value)
+                                else (StraightFlush, value)
+            | otherwise = (Straight, value)
+          rank_group groups = case length groups of
+            5 -> (High, value)
+            4 -> (OnePair, value2)
+            3 -> if length group_head == 3
+                    then (ThreeKind, value2)
+                    else (TwoPairs, value2)
+            2 -> if length group_head == 3
+                    then (FullHouse, value2)
+                    else (FourKind, value2)
+            where group_head = head groups
+                  value2 = fst $ head group_head
